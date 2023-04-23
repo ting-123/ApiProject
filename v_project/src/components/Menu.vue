52f066fc548538b062651f72ecf34ca3246222e7
ting-123 <1830611238@qq.com>
2023-04-23 16:51:26 +0800

    kk

---
+++ v_project/src/components/Menu.vue
@@ -0,0 +1,67 @@
+<template>
+<el-menu
+      default-active="2"
+      class="el-menu-vertical-demo"
+      :default-openeds="['1','2','3']"
+      @open="handleOpen"
+      @close="handleClose">
+      <el-submenu index="1">
+        <template slot="title">
+          <i class="el-icon-menu"></i>
+          <span>功能区</span>
+        </template>
+
+        <el-menu-item-group>
+          <el-menu-item index="1-1">项目管理</el-menu-item>
+          <el-menu-item index="1-2">用例设计</el-menu-item>
+          <el-menu-item index="1-2">环境管理</el-menu-item>
+
+        </el-menu-item-group>
+        </el-submenu>
+
+      <el-submenu index="2">
+        <template slot="title">
+        <i class="el-icon-user"></i>
+          <span>用户区</span>
+        </template>
+
+        <el-menu-item-group>
+          <el-menu-item index="1-1">个人信息</el-menu-item>
+          <el-menu-item index="1-2">个人贡献</el-menu-item>
+          <el-menu-item index="1-2">发送消息</el-menu-item>
+          <a href="http://localhost:8081/logout/"><el-menu-item>退出</el-menu-item></a>
+          <a href="http://127.0.0.1:8008/help/"><el-menu-item>帮助</el-menu-item></a>
+        </el-menu-item-group>
+        </el-submenu>
+
+      <el-submenu index="3">
+        <template slot="title">
+        <i class="el-icon-setting"></i>
+          <span>维护区</span>
+        </template>
+
+        <el-menu-item-group>
+          <a href="http://127.0.0.1:8008/admin/"><el-menu-item>进入后台</el-menu-item></a>
+          <el-menu-item index="1-2">发布公告</el-menu-item>
+          <el-menu-item index="1-2">查看日志</el-menu-item>
+          <el-menu-item index="1-2">权限管理</el-menu-item>
+        </el-menu-item-group>
+        </el-submenu>
+
+    </el-menu>
+</template>
+
+<script>
+export default {
+  name: "Menu",
+  data(){
+    return{
+
+    }
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
---
+++ v_project/src/views/Home.vue
@@ -0,0 +1,110 @@
+<template>
+  <div id="home" style="height: 100%">
+    <el-container style="height: 100%">
+
+      <el-aside width="200px">
+        <Menu></Menu>
+      </el-aside>
+
+      <el-container>
+        <el-header style="height: 80px">
+          <img src="/static/title.gif" alt="" style="height: 60px;padding-top: 8px">
+
+        </el-header>
+        <el-main>
+          <el-card class="box-card" style="width: 500px;min-height: 300px">
+          </el-card >
+
+          <div style="float: right">
+          <el-card class="box-card" style="width: 500px;min-height: 300px;">
+
+          </el-card>
+          </div>
+
+        </el-main>
+        <el-footer style="overflow-y:auto;min-height: 160px;padding-top: 15px;text-align: left">
+          <el-card class="box-card" style="width: 100%;min-height: 90%;background: linear-gradient(to left,white,#e5ffff)">
+            <div v-for="o in tj_datas.notices" class="text item">
+              {{'平台更新日志：'+o.content}}
+            </div>
+          </el-card>
+        </el-footer>
+      </el-container>
+    </el-container>
+
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+  import Menu from '../components/Menu.vue'
+  import axios from 'axios'
+
+  export default {
+    name: 'home',
+    data(){
+      return{
+        tj_datas:{
+          notices:[]
+        }
+
+      };
+    },
+    methods:{
+
+    },
+    components: {
+      Menu,
+
+    },
+    mounted:function () {
+      axios('http://127.0.0.1:8008/get_tj_datas').then(res=>{
+        this.tj_datas = res.datas;
+      })
+    }
+  }
+</script>
+
+
+<style>
+  .el-header {
+    background-color: #c5eded;
+    color: #333;
+    text-align: center;
+    line-height: 60px;
+  }
+
+  .el-footer {
+    background-color: #f4f1f2;
+    color: #333;
+    text-align: center;
+    line-height: 60px;
+  }
+
+  .el-aside {
+    background-color: #D3DCE6;
+    color: #333;
+    text-align: center;
+    line-height: 200px;
+  }
+
+  .el-main {
+    background-color: white;
+    color: #333;
+    text-align: center;
+    line-height: 160px;
+  }
+
+  body > .el-container {
+    margin-bottom: 40px;
+  }
+
+  .el-container:nth-child(5) .el-aside,
+  .el-container:nth-child(6) .el-aside {
+    line-height: 260px;
+  }
+
+  .el-container:nth-child(7) .el-aside {
+    line-height: 320px;
+  }
+</style>
